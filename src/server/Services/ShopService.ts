825c3a0c6acb57837748aedab3de4023ace10b8e
wigillette <wgillette02@gmail.com>
2022-05-16 23:57:21 -0400

    Updated shop service

--- src/server/Services/ShopService.ts
+++ src/server/Services/ShopService.ts
@@ -1,5 +1,5 @@
 import { KnitServer as Knit } from "@rbxts/knit";
-import { ItemInfo, PACK_INFO, RARITIES, PACK_PRICES } from "../../shared/ShopData";
+import { PACK_INFO, RARITIES, PACK_PRICES } from "../../shared/ShopData";
 import { InventoryService } from "./InventoryService";
 import { GoldService } from "./GoldService";
 import SnackbarService from "./SnackbarService";
@@ -20,28 +20,12 @@ const ShopService = Knit.CreateService({
 
 	Client: {
 		// Handles client-server communication; OnServerEvent
-		GetShopData() {
-			return this.Server.GetShopData();
-		},
-		PurchaseItem(Player: Player, itemName: string, category: string) {
-			return this.Server.PurchaseItem(Player, itemName, category);
-		},
+
 		PurchasePack(Player: Player, packName: string) {
 			coroutine.wrap(() => {
 				this.Server.PurchasePack(Player, packName);
 			})();
 		},
-	},
-
-	// Get Shop Function: returns ShopData from the server
-	GetShopData() {
-		const shopMap = new Map<string, number>();
-
-		ItemInfo.forEach((item) => {
-			shopMap.set(item.Name, item.Price);
-		}); // Create a map for the item Info
-
-		return shopMap;
 	},
 
 	PurchasePack(player: Player, packName: string) {
@@ -82,42 +66,6 @@ const ShopService = Knit.CreateService({
 		} else {
 			SnackbarService.PushPlayer(player, `Insufficient gold`);
 		}
-	},
-
-	// Check if an item exists in the shop data
-	itemExists(itemName: string) {
-		let exists: item | undefined = undefined;
-		ItemInfo.forEach((item) => {
-			if (item.Name === itemName && exists === undefined) {
-				exists = item;
-			}
-		});
-
-		return exists;
-	},
-
-	PurchaseItem(player: Player, itemName: string, category: string) {
-		let response = "Error";
-		const item = this.itemExists(itemName) as item | undefined;
-		if (item) {
-			const price = item.Price; // The item's price
-			if (!InventoryService.ContainsItem(player, itemName, category)) {
-				// Check if the user already has the item
-				const userGold = GoldService.GetGold(player);
-				if (userGold >= price) {
-					// Check if the user can afford the item
-					GoldService.AddGold(player, -price);
-					InventoryService.AddToInventory(player, itemName, category);
-					response = "Purchase Successful!";
-				} else {
-					response = "Insufficient gold";
-				}
-			} else {
-				response = "Already purchased!";
-			}
-		}
-
-		return response;
 	},
 
 	// Initialize on service startup
